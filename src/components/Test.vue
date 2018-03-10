44f8784be32fb1bc9f6aa36f2702d62a71eed78e
test <toderesa97@gmail.com>
2018-03-10 21:08:46 +0100

    Playing with Vue.js

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="test">
+    <h1> {{title}} </h1>
+    <p>{{user.first_name}}</p>
+    <p v-if="show" v-text="user.address"></p>
+    <p v-else>Cannot show information</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'test',
+    data () {
+      return {
+        title: 'Hello?',
+        user: {
+          first_name: 'James',
+          address: 'New York, 25148, St. Park'
+        },
+        show: true
+
+      }
+
+    }
+  }
+
+</script>
